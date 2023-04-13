29b7fff6ed63407c768fe2a679abd345fa7d83a6
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-13 19:12:09 +0200

    build: use ts for tailwind config file
    See https://tailwindcss.com/blog/tailwindcss-v3-3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 import typography from '@tailwindcss/typography';
 import daisyui from 'daisyui';
+import type { Config } from 'tailwindcss';
 
-/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
 	plugins: [typography, daisyui]
-};
+} satisfies Config;
