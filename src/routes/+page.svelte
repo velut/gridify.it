650c3129207b0221a12cdfa89d830a2a99bc3752
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-13 19:05:03 +0200

    build: setup daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1 class="text-3xl text-red-700">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="btn btn-primary w-64 rounded-full">hello</button>
+<button class="btn btn-accent btn-outline">hello</button>
