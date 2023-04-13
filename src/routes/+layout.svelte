4a0c905f6cf5e07b8188323710e0ed29c393bae9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-13 20:45:54 +0200

    feat: add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../app.css';
+	import Footer from './Footer.svelte';
 </script>
 
 <slot />
+<Footer />
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,18 @@
+<footer class="footer p-10 bg-base-200 text-base-content">
+	<div>
+		<p>
+			Website created by <a href="https://github.com/velut" class="link">Edoardo Scibona</a>.
+		</p>
+	</div>
+
+	<div>
+		<span class="footer-title">Links</span>
+		<a href="/" class="link link-hover">Homepage</a>
+		<a href="https://github.com/velut/pic-to-grid" class="link link-hover">GitHub Project</a>
+	</div>
+
+	<div>
+		<span class="footer-title">Legal</span>
+		<a href="/privacy" class="link link-hover">Privacy Policy</a>
+	</div>
+</footer>
