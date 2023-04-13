8183a4993335c17a2d637cd5bd3ea52dc5b740bf
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-13 19:53:48 +0200

    feat: add privacy page

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Metadata from '../Metadata.svelte';
+</script>
+
+<Metadata title="Privacy Policy" description="Privacy policy for pic2grid" path="privacy" />
+
+<article class="prose lg:prose-lg">
+	<h2>Privacy Policy</h2>
+	<p>Last updated on April 13, 2023.</p>
+
+	<h3>First Party</h3>
+	<p>On this website, we do not directly collect personal information from our visitors.</p>
+
+	<h3>Hosting</h3>
+	<p>
+		This website is hosted by <a href="https://vercel.com/">Vercel</a>
+		, which may collect some data to provide its services. To learn more about the data collected by
+		Vercel, you can read their{' '}
+		<a href="https://vercel.com/legal/privacy-policy">privacy policy</a>
+		.
+	</p>
+
+	<h3>External Websites</h3>
+	<p>
+		This website may contain links to external websites not operated by us and thus with different
+		privacy policies. We recommend you to review the privacy policy of any website you may visit.
+	</p>
+
+	<h3>Contact Information</h3>
+	<p>
+		You can contact the website owner on{' '}
+		<a href="https://github.com/velut">Github</a> or by{' '}
+		<a href="mailto:info@jsdocs.io">email</a>.
+	</p>
+</article>
