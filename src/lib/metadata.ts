6a36aabb618d1e6d4107418231aaa5d47670a0e6
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-13 19:52:37 +0200

    feat: add Metadata component

---
+++ src/lib/metadata.ts
@@ -0,0 +1,7 @@
+export const pageTitle = (title: string) => {
+	return `${title} | pic2grid`;
+};
+
+export const pageUrl = (path: string) => {
+	return `https://pic2grid.vercel.app/${path}`;
+};
---
+++ src/routes/Metadata.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { pageTitle, pageUrl } from '../lib/metadata';
+
+	export let title: string;
+	export let description: string;
+	export let path: string;
+
+	$: title = pageTitle(title);
+	$: url = pageUrl(path);
+</script>
+
+<svelte:head>
+	<!-- Title and description -->
+	<title>{title}</title>
+	<meta name="description" content={description} />
+
+	<!-- OpenGraph -->
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:url" content={url} />
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+</svelte:head>
