69013f535b9bae15a9fe566e442f4967646cc5c5
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-15 19:54:10 +0200

    fix: fix files input id

--- src/routes/FilesInput.svelte
+++ src/routes/FilesInput.svelte
@@ -2,6 +2,7 @@
 	import { getFiles } from '$lib/get-files';
 	import { store } from '$lib/store';
 
+	const inputId = 'files-input';
 	const accept = 'image/*';
 
 	const loadImages = async (event: Event) => {
@@ -11,10 +12,10 @@
 </script>
 
 <div class="form-control w-full">
-	<label for="file-input" class="sr-only">Choose image files</label>
+	<label for={inputId} class="sr-only">Choose image files</label>
 	<input
-		id="files-input"
-		name="files-input"
+		id={inputId}
+		name={inputId}
 		type="file"
 		multiple
 		{accept}
