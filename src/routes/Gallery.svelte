bf74d741f6c4c3362bee0548ae2b612748eb0420
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-15 02:03:41 +0200

    feat: add gallery empty state

--- src/routes/Gallery.svelte
+++ src/routes/Gallery.svelte
@@ -1,1 +1,16 @@
-<div class="grow border p-1">gallery</div>
+<script lang="ts">
+	type Image = { filename: string; url: string };
+	const images: Image[] = [];
+</script>
+
+<div class="card card-compact grow bg-base-300 shadow">
+	<div class="card-body gap-4">
+		{#each images as image}
+			<div>{image.url}</div>
+		{:else}
+			<div class="flex justify-center items-center h-full">
+				<div class="btn btn-disabled btn-lg bg-base-300">No Images</div>
+			</div>
+		{/each}
+	</div>
+</div>
