0b9248c5ad9b397b7e7f91a0e051984992a6c31e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-17 02:19:09 +0200

    feat: display loaded images

--- src/routes/GalleryImage.svelte
+++ src/routes/GalleryImage.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-	export let image: string;
+	import type { Image } from '$lib/image';
+
+	export let image: Image;
 </script>
 
-<div>{image}</div>
+<a href={image.url} target="_blank" class="block">
+	<img src={image.url} alt="" class="h-48 w-48 rounded object-cover" />
+</a>
