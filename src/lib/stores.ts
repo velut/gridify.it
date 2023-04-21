cb4e63c2a1105bc795e4b1b88329197fd8e013d2
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-21 19:13:10 +0200

    refactor: improve types of input images derived stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,10 +1,11 @@
 import { fileToImage } from '$lib/file-to-image';
-import { asyncDerived, writable } from '@square/svelte-store';
+import { asyncDerived, writable, type LoadState, type Readable } from '@square/svelte-store';
 
 export const files = writable<File[]>([]);
 
-export const { store: inputImages, state: inputImagesState } = asyncDerived(
-	files,
-	async ($files) => $files.map(fileToImage),
-	{ trackState: true }
-);
+const _inputImages = asyncDerived(files, async ($files) => $files.map(fileToImage), {
+	trackState: true
+});
+
+export const inputImages = _inputImages.store;
+export const inputImagesState = _inputImages.state as Readable<LoadState>;
