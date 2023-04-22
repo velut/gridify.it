8d9dc1772471fcec1778787c1d7c2148368e80e2
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-22 18:33:20 +0200

    feat: add download images functionality

--- src/routes/DownloadImagesButton.svelte
+++ src/routes/DownloadImagesButton.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
-	let disabled = true;
+	import { outputImages, outputImagesState } from '$lib/stores';
+	import { downloadZip } from 'client-zip';
+	import { saveAs } from 'file-saver';
+
+	$: ({ isLoaded } = $outputImagesState);
+	$: images = $outputImages;
+	$: disabled = !isLoaded || images.length === 0;
+
+	const downloadImages = async () => {
+		const archive = await downloadZip(images.map((image) => image.file)).blob();
+		saveAs(archive, 'pic2grid_images.zip');
+	};
 </script>
 
-<button class="btn-outline btn w-full" {disabled}>Download Images</button>
+<button class="btn-outline btn w-full" on:click={downloadImages} {disabled}>Download Images</button>
