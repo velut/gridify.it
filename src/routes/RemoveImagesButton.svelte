8582d505dfaf74644cdf84387a84d05df9947a25
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-22 20:55:50 +0200

    feat: add remove images button

---
+++ src/routes/RemoveImagesButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { files } from '$lib/stores';
+
+	const resetFiles = async () => {
+		$files = [];
+	};
+
+	$: disabled = $files.length === 0;
+</script>
+
+<button class="btn-outline btn-sm btn w-full hover:btn-error" on:click={resetFiles} {disabled}
+	>Remove Images</button
+>
