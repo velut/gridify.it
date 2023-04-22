10f3ee9467b1e2f8eafbd567695bd1bdd4767e99
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-22 02:17:06 +0200

    feat: add render options form values schema

--- src/lib/render-options-form.ts
+++ src/lib/render-options-form.ts
@@ -1,44 +1,79 @@
 import { hexColorRegex } from '$lib/hex-color-regex';
 import { z } from 'zod';
 
+const toInt = (value: string) => parseInt(value, 10);
+
+const isZeroOrMore = (value: string) => {
+	const number = toInt(value);
+	return !isNaN(number) && number >= 0;
+};
+
+const isOneOrMore = (value: string) => {
+	const number = toInt(value);
+	return !isNaN(number) && number >= 1;
+};
+
+const gridTypeSchema = z.enum(['none', 'inner', 'outer', 'full']);
+
+const gridStrokeSizeSchema = z
+	.string()
+	.refine(isOneOrMore, { message: 'Grid stroke size must be an integer number like 1, 2, 3' });
+
+const gridStrokeColorSchema = z.string().regex(hexColorRegex);
+
+const gridSchema = z.object({
+	type: gridTypeSchema,
+	strokeSize: gridStrokeSizeSchema,
+	strokeColor: gridStrokeColorSchema
+});
+
+const gridValuesSchema = z.object({
+	type: gridTypeSchema,
+	strokeSize: gridStrokeSizeSchema.transform(toInt),
+	strokeColor: gridStrokeColorSchema
+});
+
+const cellSizeSchema = z
+	.string()
+	.refine(isOneOrMore, { message: 'Cell size must be an integer number like 1, 2, 3' });
+
+const cellScaleSchema = z
+	.string()
+	.refine(isOneOrMore, { message: 'Cell scale multiplier must be an integer number like 1, 2, 3' });
+
+const cellRadiusSchema = z.string().refine(isZeroOrMore, {
+	message: 'Cell border radius must be an integer number like 0, 1, 2, 3'
+});
+
+const cellSchema = z.object({
+	size: cellSizeSchema,
+	scale: cellScaleSchema,
+	radius: cellRadiusSchema
+});
+
+const cellValuesSchema = z.object({
+	size: cellSizeSchema.transform(toInt),
+	scale: cellScaleSchema.transform(toInt),
+	radius: cellRadiusSchema.transform(toInt)
+});
+
+const pixelFullyOpaqueSchema = z.boolean();
+
+const pixelSchema = z.object({
+	fullyOpaque: pixelFullyOpaqueSchema
+});
+
 export const renderOptionsFormSchema = z.object({
-	grid: z.object({
-		type: z.enum(['none', 'inner', 'outer', 'full']),
-		strokeSize: z.string().refine(
-			(value) => {
-				const number = parseInt(value, 10);
-				return !isNaN(number) && number >= 1;
-			},
-			{ message: 'Grid stroke size must be an integer number like 1, 2, 3' }
-		),
-		strokeColor: z.string().regex(hexColorRegex)
-	}),
-	cell: z.object({
-		size: z.string().refine(
-			(value) => {
-				const number = parseInt(value, 10);
-				return !isNaN(number) && number >= 1;
-			},
-			{ message: 'Cell size must be an integer number like 1, 2, 3' }
-		),
-		scale: z.string().refine(
-			(value) => {
-				const number = parseInt(value, 10);
-				return !isNaN(number) && number >= 1;
-			},
-			{ message: 'Cell scale multiplier must be an integer number like 1, 2, 3' }
-		),
-		radius: z.string().refine(
-			(value) => {
-				const number = parseInt(value, 10);
-				return !isNaN(number) && number >= 0;
-			},
-			{ message: 'Cell border radius must be an integer number like 0, 1, 2, 3' }
-		)
-	}),
-	pixel: z.object({
-		fullyOpaque: z.boolean()
-	})
+	grid: gridSchema,
+	cell: cellSchema,
+	pixel: pixelSchema
+});
+
+export const renderOptionsFormValuesSchema = z.object({
+	grid: gridValuesSchema,
+	cell: cellValuesSchema,
+	pixel: pixelSchema
 });
 
 export type RenderOptionsForm = z.infer<typeof renderOptionsFormSchema>;
+export type RenderOptionsFormValues = z.infer<typeof renderOptionsFormValuesSchema>;
