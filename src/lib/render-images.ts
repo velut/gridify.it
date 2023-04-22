8227e9e1097f5698712bda6498f6893ca5d29000
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-23 01:02:00 +0200

    feat: update renderImages

--- src/lib/render-images.ts
+++ src/lib/render-images.ts
@@ -1,10 +1,24 @@
 import { fileToImage } from '$lib/file-to-image';
 import type { Image } from '$lib/image';
 import type { RenderOptions } from '$lib/render-options';
+import pLimit from 'p-limit';
 
-export const renderImages = async (inputImages: Image[], renderOptions: RenderOptions | null) => {
+const limit = pLimit(10);
+
+const renderImage = async (inputImage: Image, renderOptions: RenderOptions): Promise<Image> => {
+	return fileToImage(inputImage.file);
+};
+
+export const renderImages = async (
+	inputImages: Image[],
+	renderOptions: RenderOptions | null
+): Promise<Image[]> => {
 	if (!renderOptions) {
 		return inputImages.map((image) => fileToImage(image.file));
 	}
-	return [];
+	return (
+		await Promise.allSettled(
+			inputImages.map((image) => limit(() => renderImage(image, renderOptions)))
+		)
+	).flatMap((result) => (result.status === 'fulfilled' ? result.value : []));
 };
