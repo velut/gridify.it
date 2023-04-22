b727d7165409b1e33a368ce1894fbd2799edede9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-22 18:44:55 +0200

    feat: disable render button when there are no input images

--- src/routes/RenderOptionsFormRenderImagesButton.svelte
+++ src/routes/RenderOptionsFormRenderImagesButton.svelte
@@ -1,1 +1,9 @@
-<button type="submit" class="btn-outline btn w-full">Render Images</button>
+<script lang="ts">
+	import { inputImages, inputImagesState } from '$lib/stores';
+
+	$: ({ isLoaded } = $inputImagesState);
+	$: images = $inputImages;
+	$: disabled = !isLoaded || images.length === 0;
+</script>
+
+<button type="submit" class="btn-outline btn w-full" {disabled}>Render Images</button>
