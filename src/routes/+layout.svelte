beaac6fa149838cc2677ac1eca479527b4ab6f9a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-05-15 19:23:45 +0200

    chore: remove unused tippy.js dep

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
-	import 'tippy.js/dist/tippy.css';
 	import '../app.css';
 </script>
 
