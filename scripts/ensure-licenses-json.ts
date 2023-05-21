5c9c4fde77ee22a5efa9db2124ac60cda633d85e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-05-21 20:45:03 +0200

    build: ensure that licenses.json exists before building

---
+++ scripts/ensure-licenses-json.ts
@@ -0,0 +1,13 @@
+import { existsSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+// Run this script from the root of the project.
+const filePath = join(process.cwd(), 'src/licenses.json');
+
+const ensureLicensesJson = () => {
+	if (!existsSync(filePath)) {
+		writeFileSync(filePath, '{}', 'utf8');
+	}
+};
+
+ensureLicensesJson();
