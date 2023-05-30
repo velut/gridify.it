11fb7cc9621f3c6627fe8636b788f9d29849162e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-05-30 19:12:40 +0200

    refactor: reference project name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Gallery from '$lib/components/Gallery.svelte';
 	import Metadata from '$lib/components/Metadata.svelte';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { projectName } from '$lib/metadata';
 	import { renderOptionsSuperForm } from '$lib/stores';
 	import type { PageData } from './$types';
 
@@ -12,7 +13,7 @@
 
 <Metadata
 	title="Awesome grids for your images"
-	description="With pic2grid you can easily add a customizable grid to your images or pixel art creations. You can use different grid types and line colors, resize images, and round the corners of grid cells."
+	description="With {projectName} you can easily add a customizable grid to your images or pixel art creations. You can use different grid types and line colors, resize images, and round the corners of grid cells."
 />
 
 <div class="container mx-auto flex h-full flex-col gap-6 p-6 sm:flex-row sm:items-start">
