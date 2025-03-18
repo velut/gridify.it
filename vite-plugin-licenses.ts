82583e521a3ec16754440df76a8aae67cfed8a2a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-19 00:42:14 +0100

    build: update licenses plugin

--- vite-plugin-licenses.ts
+++ vite-plugin-licenses.ts
@@ -1,7 +1,6 @@
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 import rollupPluginLicense from 'rollup-plugin-license';
-import type { PluginOption } from 'vite';
-import packageJson from './package.json';
+import packageJson from './package.json' with { type: 'json' };
 
 type License = {
 	name: string;
@@ -11,86 +10,87 @@ type License = {
 	url: string;
 };
 
-const registryUrl = 'https://www.npmjs.com/package';
-
-const manualLicenses: License[] = [
-	{
-		name: '@tailwindcss/typography',
-		version: packageJson.devDependencies['@tailwindcss/typography'].replace('^', ''),
-		license: 'MIT',
-		licenseText: fs.readFileSync('./node_modules/@tailwindcss/typography/LICENSE').toString(),
-		url: 'https://github.com/tailwindlabs/tailwindcss-typography'
-	},
-	{
-		name: 'daisyui',
-		version: packageJson.devDependencies['daisyui'].replace('^', ''),
-		license: 'MIT',
-		licenseText: fs.readFileSync('./node_modules/daisyui/LICENSE').toString(),
-		url: 'https://daisyui.com'
-	},
-	{
-		name: 'modern-normalize',
-		version: packageJson.devDependencies['modern-normalize'].replace('^', ''),
-		license: 'MIT',
-		licenseText: fs.readFileSync('./node_modules/modern-normalize/license').toString(),
-		url: 'https://github.com/sindresorhus/modern-normalize'
-	},
-	{
-		name: 'tailwindcss',
-		version: packageJson.devDependencies['tailwindcss'].replace('^', ''),
-		license: 'MIT',
-		licenseText: fs.readFileSync('./node_modules/tailwindcss/LICENSE').toString(),
-		url: 'https://tailwindcss.com'
-	},
-	{
-		name: 'twemoji',
-		version: '14.0.2',
-		license: 'MIT, CC-BY-4.0',
-		licenseText: fs.readFileSync('./third-party-licenses/twemoji/license.txt').toString(),
-		url: 'https://twemoji.twitter.com/'
-	}
-];
-
-export const licenses = (): PluginOption => {
+export async function licenses() {
 	// "Falsy plugins will be ignored, which can be used to easily activate or deactivate plugins."
 	// See https://vitejs.dev/guide/api-plugin.html#plugins-config.
-	if (process.env.GENERATE_LICENSES !== 'true') {
+	if (process.env.NODE_ENV !== 'production') {
 		return false;
 	}
+
+	const manualDeps: License[] = [
+		{
+			name: '@tailwindcss/typography',
+			version: packageJson.devDependencies['@tailwindcss/typography'].replace('^', ''),
+			license: 'MIT',
+			licenseText: await fs.readFile('./node_modules/@tailwindcss/typography/LICENSE', {
+				encoding: 'utf-8'
+			}),
+			url: 'https://github.com/tailwindlabs/tailwindcss-typography'
+		},
+		{
+			name: 'daisyui',
+			version: packageJson.devDependencies['daisyui'].replace('^', ''),
+			license: 'MIT',
+			licenseText: await fs.readFile('./node_modules/daisyui/LICENSE', { encoding: 'utf-8' }),
+			url: 'https://daisyui.com'
+		},
+		{
+			name: 'tailwindcss',
+			version: packageJson.devDependencies['tailwindcss'].replace('^', ''),
+			license: 'MIT',
+			licenseText: await fs.readFile('./node_modules/tailwindcss/LICENSE', { encoding: 'utf-8' }),
+			url: 'https://tailwindcss.com'
+		},
+		{
+			name: 'Noto Emoji',
+			version: '16.0',
+			license: 'Apache-2.0',
+			licenseText: await fs.readFile('./third-party-licenses/noto-emoji/license.txt', {
+				encoding: 'utf-8'
+			}),
+			url: 'https://github.com/googlefonts/noto-emoji'
+		}
+	];
 
 	// See https://github.com/vitejs/vite/discussions/7722 and
 	// https://github.com/mjeanroy/rollup-plugin-license and
 	// https://github.com/mjeanroy/rollup-plugin-license/issues/1362.
-	return {
-		...rollupPluginLicense({
-			thirdParty: {
-				includePrivate: false,
-				allow: {
-					test: 'MIT OR ISC OR 0BSD',
-					// Disabled because `sveltekit-superforms` requires `@arktype/util`
-					// which has no license in package.json but is MIT licensed as a project.
-					// failOnUnlicensed: true,
-					failOnViolation: true
-				},
-				output: {
-					file: 'src/licenses.json',
-					template: (dependencies) => {
-						const foundLicenses = dependencies.map(
-							(dep): License => ({
-								name: dep.name ?? '',
-								version: dep.version ?? '',
-								license: dep.license ?? '',
-								licenseText: dep.licenseText ?? '',
-								url: dep.homepage || (dep.name ? `${registryUrl}/${dep.name}` : '')
-							})
-						);
-						const allLicenses: License[] = [...manualLicenses, ...foundLicenses].sort((a, b) =>
-							a.name.localeCompare(b.name)
-						);
-						return JSON.stringify(allLicenses, null, 2);
-					}
+	return rollupPluginLicense({
+		thirdParty: {
+			includePrivate: false,
+			allow: {
+				test: 'MIT OR ISC OR 0BSD OR Apache-2.0',
+				failOnUnlicensed: true,
+				failOnViolation: true
+			},
+			output: {
+				file: 'static/third-party-licenses.txt',
+				template: (dependencies) => {
+					const foundLicenses = dependencies.map(
+						(dep): License => ({
+							name: dep.name ?? '',
+							version: dep.version ?? '',
+							license: dep.license ?? '',
+							licenseText: dep.licenseText ?? '',
+							url: dep.homepage || (dep.name ? `https://www.npmjs.com/package/${dep.name}` : '')
+						})
+					);
+					return [
+						'',
+						...[...manualDeps, ...foundLicenses]
+							.sort((a, b) => a.name.localeCompare(b.name))
+							.map((dep) =>
+								[
+									`${dep.name} - v${dep.version} - ${dep.license}`,
+									dep.url,
+									'',
+									dep.licenseText.trim()
+								].join('\n')
+							),
+						''
+					].join(`\n\n${'='.repeat(50)}\n\n`);
 				}
 			}
-		})
-	} as unknown as PluginOption;
-};
+		}
+	});
+}
