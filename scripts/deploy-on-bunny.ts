26418434da2c32bbb787154bc1a64c418b5979ba
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-19 01:07:01 +0100

    build: update deploy on bunny script

--- scripts/deploy-on-bunny.ts
+++ scripts/deploy-on-bunny.ts
@@ -1,124 +1,86 @@
-import 'dotenv/config';
-import { hash } from 'hasha';
-import klaw from 'klaw';
-import fs from 'node:fs';
-import path from 'pathe';
+import { createHash } from 'node:crypto';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
-// Run this script from the root of the project.
+try {
+	console.log('deploying to bunny.net...');
+	await deploy();
+	console.log('done deploying');
+	process.exit(0);
+} catch (e) {
+	console.error('error deploying:');
+	console.error(e);
+	process.exit(1);
+}
 
-type PullZonesList = { Items: { Id: string; Name: string }[] };
-
-const deploy = async () => {
+async function deploy() {
 	const apiKey = process.env.BUNNY_API_KEY!;
+	const pullZoneId = process.env.BUNNY_PULL_ZONE_ID!;
 	const storageZoneHostname = process.env.BUNNY_STORAGE_ZONE_HOSTNAME!;
 	const storageZoneName = process.env.BUNNY_STORAGE_ZONE_NAME!;
 	const storageZonePassword = process.env.BUNNY_STORAGE_ZONE_PASSWORD!;
-	const pullZoneName = process.env.BUNNY_PULL_ZONE_NAME!;
+	const storageZoneEndpoint = `https://${storageZoneHostname}/${storageZoneName}`;
+
+	// Script must be run from the project's root.
 	const buildDir = path.join(process.cwd(), 'build');
 
-	console.log('Deploying to Bunny.net...');
+	await clearStorageZone(storageZoneEndpoint, storageZonePassword);
+	await uploadFiles(buildDir, storageZoneEndpoint, storageZonePassword);
+	await purgePullZoneCache(pullZoneId, apiKey);
+}
 
-	const clearStorageZone = async () => {
-		const endpoint = `https://${storageZoneHostname}/${storageZoneName}/`;
+async function clearStorageZone(endpoint: string, password: string) {
+	console.log('clearStorageZone: clearing storage zone');
 
-		// Clear the storage zone.
-		// THIS DELETES ALL THE FILES IN THE STORAGE ZONE!
-		await fetch(endpoint, {
-			method: 'DELETE',
-			headers: { AccessKey: storageZonePassword }
-		});
+	// DELETE ALL FILES IN THE STORAGE ZONE.
+	const storageRootDir = `${endpoint}/`;
+	await fetch(storageRootDir, { method: 'DELETE', headers: { AccessKey: password } });
 
-		// Wait a little.
-		await new Promise((r) => setTimeout(r, 500));
-
-		// Check if the storage zone was cleared.
-		const res = await fetch(endpoint, {
-			method: 'GET',
-			headers: { accept: 'application/json', AccessKey: storageZonePassword }
-		});
-		const data = await res.json();
-		const isEmptyArray = Array.isArray(data) && data.length === 0;
-		if (!isEmptyArray) {
-			throw new Error('clearStorageZone: failed to clear storage zone');
-		}
-		console.log('clearStorageZone: cleared storage zone');
-	};
-
-	await clearStorageZone();
-
-	const uploadFilesToStorageZone = async () => {
-		let numFiles = 0;
-		for await (const file of klaw(buildDir)) {
-			if (file.stats.isDirectory()) {
-				continue;
-			}
-			const filePath = path.normalize(file.path);
-			const targetPath = filePath.replace(`${buildDir}/`, '');
-			const fileData = fs.readFileSync(file.path);
-			const fileHash = (await hash(fileData, { algorithm: 'sha256' })).toUpperCase();
-			const res = await fetch(`https://${storageZoneHostname}/${storageZoneName}/${targetPath}`, {
-				method: 'PUT',
-				headers: {
-					Checksum: fileHash,
-					'content-type': 'application/octet-stream',
-					AccessKey: storageZonePassword
-				},
-				body: fileData
-			});
-			if (res.status >= 400) {
-				throw new Error(`uploadFilesToStorageZone: failed to upload file: ${targetPath}`);
-			}
-			console.log(`uploadFilesToStorageZone: uploaded file: ${targetPath}`);
-			numFiles += 1;
-		}
-		console.log(`uploadFilesToStorageZone: uploaded ${numFiles} files`);
-	};
-
-	await uploadFilesToStorageZone();
-
-	const getPullZoneId = async () => {
-		const res = await fetch(
-			`https://api.bunny.net/pullzone?page=1&perPage=1000&search=${pullZoneName}&includeCertificate=false`,
-			{
-				method: 'GET',
-				headers: { accept: 'application/json', AccessKey: apiKey }
-			}
-		);
-		if (res.status >= 400) {
-			throw new Error('getPullZoneId: failed to get id for pull zone');
-		}
-		const pullZones = (await res.json()) as PullZonesList;
-		const pullZoneId = pullZones.Items.find((item) => item.Name === pullZoneName)?.Id;
-		if (!pullZoneId) {
-			throw new Error('getPullZoneId: cannot find pull zone in list');
-		}
-		console.log('getPullZoneId: found pull zone id');
-		return pullZoneId;
-	};
-
-	const pullZoneId = await getPullZoneId();
-
-	const purgePullZoneCache = async () => {
-		const res = await fetch(`https://api.bunny.net/pullzone/${pullZoneId}/purgeCache`, {
-			method: 'POST',
-			headers: { 'content-type': 'application/json', AccessKey: apiKey }
-		});
-		if (res.status >= 400) {
-			throw new Error('purgePullZoneCache: failed to purge pull zone cache');
-		}
-		console.log('purgePullZoneCache: purged pull zone cache');
-	};
-
-	await purgePullZoneCache();
-};
-
-deploy()
-	.then(() => {
-		console.log('Done deploying.');
-		process.exit(0);
-	})
-	.catch((e) => {
-		console.error('Error deploying:');
-		console.error(e);
-		process.exit(1);
+	// Check if the storage zone was cleared.
+	await new Promise((r) => setTimeout(r, 500));
+	const res = await fetch(storageRootDir, {
+		method: 'GET',
+		headers: { accept: 'application/json', AccessKey: password }
 	});
+	const data = await res.json();
+	const isEmptyArray = Array.isArray(data) && data.length === 0;
+	if (!isEmptyArray) {
+		throw new Error('clearStorageZone: failed to clear storage zone');
+	}
+	console.log('clearStorageZone: cleared storage zone');
+}
+
+async function uploadFiles(buildDir: string, endpoint: string, password: string) {
+	const entries = await fs.readdir(buildDir, { recursive: true, withFileTypes: true });
+	const files = entries.filter((entry) => entry.isFile());
+	let uploadedFilesCount = 0;
+	for (const file of files) {
+		const fsPath = path.join(file.parentPath, file.name);
+		const relativePath = fsPath.replace(`${buildDir}/`, '');
+		const storagePath = `${endpoint}/${relativePath}`;
+		const data = await fs.readFile(fsPath);
+		const hash = createHash('sha256').update(data).digest('hex').toUpperCase();
+		const res = await fetch(storagePath, {
+			method: 'PUT',
+			headers: { Checksum: hash, 'content-type': 'application/octet-stream', AccessKey: password },
+			body: data
+		});
+		if (res.status >= 400) {
+			throw new Error(`uploadFiles: failed to upload file: ${relativePath}`);
+		}
+		console.log(`uploadFiles: uploaded: ${relativePath}`);
+		uploadedFilesCount += 1;
+	}
+	console.log(`uploadFiles: uploaded ${uploadedFilesCount} files`);
+}
+
+async function purgePullZoneCache(pullZoneId: string, apiKey: string) {
+	const res = await fetch(`https://api.bunny.net/pullzone/${pullZoneId}/purgeCache`, {
+		method: 'POST',
+		headers: { 'content-type': 'application/json', AccessKey: apiKey }
+	});
+	if (res.status >= 400) {
+		throw new Error('purgePullZoneCache: failed to purge pull zone cache');
+	}
+	console.log('purgePullZoneCache: purged pull zone cache');
+}
