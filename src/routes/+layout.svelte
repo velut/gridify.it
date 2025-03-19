3266e82400ab90f3b39326ff9207afe645b4e0e6
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-19 18:52:51 +0100

    feat: update root layout
    Add metadata head tags.
    Update layout style.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,52 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 
 	let { children } = $props();
+
+	let title = $derived(`${page.data.title} | gridify.it`);
+	let description = $derived(page.data.description);
+	let url = $derived(page.url.toString());
+	let ogImageUrl = $derived(new URL('/twitter_card.jpg', page.url).toString());
+	const ogImageAlt = 'Promotional image for gridify.it';
 </script>
 
-<div class="flex h-screen flex-col">
-	<Navbar />
+<svelte:head>
+	<!-- Title and description -->
+	<title>{title}</title>
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
 
-	<div class="grow">
+	<!-- Canonical URL -->
+	<link rel="canonical" href={url} />
+
+	<!-- OpenGraph -->
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:url" content={url} />
+	<meta property="og:image" content={ogImageUrl} />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="628" />
+	<meta property="og:image:alt" content={ogImageAlt} />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:site_name" content="gridify.it" />
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content={ogImageUrl} />
+	<meta name="twitter:image:alt" content={ogImageAlt} />
+	<meta name="twitter:creator" content="@EdoardoScibona" />
+</svelte:head>
+
+<div class="grid min-h-svh auto-cols-fr grid-rows-[auto_1fr_auto]">
+	<Navbar />
+	<div>
 		{@render children()}
 	</div>
-
 	<Footer />
 </div>
