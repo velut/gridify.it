c95b645aaf0a853a02de72a80e6b50f3a817b1db
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-28 17:17:02 +0100

    feat: update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,23 @@
+export type RenderOptions = {
+	grid: {
+		type: GridType;
+		color: string;
+		lines: {
+			size: number;
+		};
+		cell: {
+			shape: GridCellShape;
+			width: number;
+			height: number;
+			scale: number;
+			cornerRadius: number;
+		};
+	};
+	opacity: PixelOpacity;
+};
+
 export type GridType = 'full' | 'lines' | 'border' | 'none';
-export type GridShape = 'square' | 'rectangle';
-export type Opacity = 'preserve' | 'opaque';
+
+export type GridCellShape = 'square' | 'rectangle';
+
+export type PixelOpacity = 'preserve' | 'opaque';
