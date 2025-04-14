fdae960ef62237316c4907ed7d6a45a8eb8fe4b2
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-04-14 19:35:04 +0200

    refactor: use let

--- src/lib/revoke-object-urls.ts
+++ src/lib/revoke-object-urls.ts
@@ -2,10 +2,10 @@ import type { Image } from '$lib/types';
 
 export function revokeObjectUrls(images: Image[]) {
 	if (images.length === 0) return;
-	const urls = images.map(({ url }) => url);
+	let urls = images.map(({ url }) => url);
 
 	setTimeout(() => {
-		for (const url of urls) {
+		for (let url of urls) {
 			URL.revokeObjectURL(url);
 		}
 	}, 10 * 1000);
