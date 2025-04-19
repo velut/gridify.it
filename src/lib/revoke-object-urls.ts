4dc09cb0e529eee95cdf5fec16904316f5026228
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-04-19 20:11:39 +0200

    chore: formatting

--- src/lib/revoke-object-urls.ts
+++ src/lib/revoke-object-urls.ts
@@ -3,7 +3,6 @@ import type { Image } from '$lib/types';
 export function revokeObjectUrls(images: Image[]) {
 	if (images.length === 0) return;
 	let urls = images.map(({ url }) => url);
-
 	setTimeout(() => {
 		for (let url of urls) {
 			URL.revokeObjectURL(url);
