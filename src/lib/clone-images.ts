264d2575de5c3c020ad3ef2e7dcbc56c92dbd3de
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-04-25 01:07:05 +0200

    fix: correctly clone file

--- src/lib/clone-images.ts
+++ src/lib/clone-images.ts
@@ -2,8 +2,11 @@ import type { Image } from '$lib/types';
 
 export function cloneImages(images: Image[]): Image[] {
 	// Clone the images to prevent revoking the original object URLs.
-	return images.map((image) => ({
-		file: new File([image.file], image.file.name, { type: image.file.type }),
-		url: URL.createObjectURL(image.file)
-	}));
+	return images.map((image) => {
+		let file = new File([image.file], image.file.name, {
+			type: image.file.type,
+			lastModified: image.file.lastModified
+		});
+		return { file, url: URL.createObjectURL(file) };
+	});
 }
