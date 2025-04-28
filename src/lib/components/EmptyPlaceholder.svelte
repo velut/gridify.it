0671cfa25817f226d9599c7e6e0a277abbda53b5
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-04-28 23:25:34 +0200

    feat: add empty placeholder

---
+++ src/lib/components/EmptyPlaceholder.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Card from '$lib/components/Card.svelte';
+	import MaterialSymbolsImageSearchRounded from '~icons/material-symbols/image-search-rounded';
+</script>
+
+<Card>
+	<div class="text-base-content/50 flex items-center justify-center gap-2 md:flex-col">
+		<MaterialSymbolsImageSearchRounded class="size-6 md:size-12" />
+		<span class="text-sm font-bold md:hidden">Upload an image to start</span>
+		<span class="hidden font-bold md:block md:max-w-96 md:text-center md:text-balance"
+			>Upload or drag and drop an image on the page to start</span
+		>
+	</div>
+</Card>
