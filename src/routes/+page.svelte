5c93f5ff57d402cd70e1a88e482bb7a21cf95861
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-04-28 02:48:53 +0200

    refactor: update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,10 @@
 <script lang="ts">
 	import { AppState, setAppStateContext } from '$lib/app-state.svelte';
-	import Card from '$lib/components/Card.svelte';
-	import DownloadImagesButton from '$lib/components/DownloadImagesButton.svelte';
-	import RemoveImagesButton from '$lib/components/RemoveImagesButton.svelte';
-	import RenderImagesButton from '$lib/components/RenderImagesButton.svelte';
-	import RenderOptionsForm from '$lib/components/RenderOptionsForm.svelte';
-	import ResetRenderOptionsButton from '$lib/components/ResetRenderOptionsButton.svelte';
-	import UploadImagesButton from '$lib/components/UploadImagesButton.svelte';
+	import GridifyApp from '$lib/components/GridifyApp.svelte';
 
 	setAppStateContext(new AppState());
 </script>
 
-<div class="grid gap-4 overflow-hidden p-4">
-	<Card>
-		<UploadImagesButton />
-		<RemoveImagesButton />
-	</Card>
-
-	<Card>
-		<RenderOptionsForm />
-		<ResetRenderOptionsButton />
-	</Card>
-
-	<Card>
-		<RenderImagesButton />
-		<DownloadImagesButton />
-	</Card>
+<div class="p-4">
+	<GridifyApp />
 </div>
