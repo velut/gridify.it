ca88821e9f3a216ae72c566f610873f9abc0dc73
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-04-28 02:41:45 +0200

    feat: add gridify app component

---
+++ src/lib/components/GridifyApp.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import ActionsSidebar from '$lib/components/ActionsSidebar.svelte';
+	import Card from '$lib/components/Card.svelte';
+</script>
+
+<div class="grid gap-4 md:grid-cols-[auto_minmax(0,1fr)]">
+	<ActionsSidebar />
+
+	<Card>
+		<div>TODO: Gallery</div>
+	</Card>
+</div>
