867a3dadf2cde0a91031e5ae2dc6cbe689e960ae
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-05-01 00:29:47 +0200

    feat: add PreviewMode

---
+++ src/lib/components/PreviewMode.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { getAppStateContext } from '$lib/app-state.svelte';
+	import MaterialSymbolsGalleryThumbnailRounded from '~icons/material-symbols/gallery-thumbnail-rounded';
+
+	let { preview } = getAppStateContext();
+</script>
+
+<fieldset class="fieldset text-sm">
+	<legend class="fieldset-legend">
+		<MaterialSymbolsGalleryThumbnailRounded class="size-6" />
+		Preview mode
+	</legend>
+	<select class="select" bind:value={preview.mode}>
+		<option value="pixel-art">Pixel art</option>
+		<option value="high-res">High resolution images</option>
+	</select>
+	<span class="label whitespace-normal"
+		>Previews may contain visual artifacts, click on an image to see it at full size</span
+	>
+</fieldset>
