a093a6374b1c5f5e84c822d931303df47e2961db
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-04-30 17:37:23 +0200

    feat: add Ctrl+Z hotkey for undo render images

--- src/lib/components/UndoRenderImagesButton.svelte
+++ src/lib/components/UndoRenderImagesButton.svelte
@@ -3,13 +3,25 @@
 	import MaterialSymbolsResetImageRounded from '~icons/material-symbols/reset-image-rounded';
 
 	let { images } = getAppStateContext();
+	let button: HTMLButtonElement;
+
+	function handleHotkey(e: KeyboardEvent) {
+		if (e.ctrlKey && e.key.toLowerCase() === 'z' && !button.disabled) {
+			e.preventDefault();
+			button.click();
+		}
+	}
 </script>
+
+<svelte:window onkeydown={handleHotkey} />
 
 <button
 	type="button"
 	class="btn btn-error btn-outline w-full"
 	onclick={() => images.undoRender()}
 	disabled={!images.hasInputImages()}
+	bind:this={button}
+	title="Undo render images [Ctrl+Z]"
 >
 	<MaterialSymbolsResetImageRounded class="size-6" />
 	Undo render images
