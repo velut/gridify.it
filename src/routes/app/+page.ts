1df831afcecba6be5bb2062807a52316fd3dc969
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-05-13 15:36:15 +0200

    feat: move app to app route

---
+++ src/routes/app/+page.ts
@@ -0,0 +1,7 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = () => ({
+	title: 'Awesome grids for your images',
+	description:
+		'Add a customizable grid to your images, photos, reference pictures, or pixel art designs. Use various grid types and line colors, resize images, and round the corners of grid cells.'
+});
