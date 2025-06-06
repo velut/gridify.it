e61853cef904a46e3e75b53386b49d865d958a90
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-06-06 20:04:48 +0200

    refactor: remove app from homepage route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,4 @@
 <script lang="ts">
-	import { AppState, setAppStateContext } from '$lib/app-state.svelte';
-	import Footer from '$lib/components/Footer.svelte';
-	import GridifyApp from '$lib/components/GridifyApp.svelte';
-
-	setAppStateContext(new AppState());
 </script>
 
-<div class="grid min-h-screen grid-rows-[minmax(0,1fr)_auto] lg:h-full lg:min-h-auto">
-	<GridifyApp />
-	<Footer />
-</div>
-
-<style>
-	/* lg breakpoint. */
-	@media (width >= 64rem) {
-		:global(body) {
-			overflow: hidden;
-			height: 100vh;
-		}
-	}
-</style>
+<div class="">Home</div>
