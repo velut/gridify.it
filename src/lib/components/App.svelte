1e3fab67b43396ae303920aae92b712b7d621ad9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-06-18 21:05:56 +0200

    refactor: rename to AppRenderOptions

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { getAppStateContext } from '$lib/app-state.svelte';
 	import AppActions from '$lib/components/AppActions.svelte';
+	import AppRenderOptions from '$lib/components/AppRenderOptions.svelte';
 	import Preview from '$lib/components/Preview.svelte';
 	import PreviewPlaceholder from '$lib/components/PreviewPlaceholder.svelte';
-	import RenderOptions from '$lib/components/RenderOptions.svelte';
 
 	let { images } = getAppStateContext();
 </script>
@@ -11,7 +11,7 @@
 <main class="grid gap-4 overflow-y-auto p-4 lg:grid-cols-[auto_minmax(0,1fr)]">
 	<div class="grid gap-4 overflow-y-auto sm:grid-cols-[auto_auto]">
 		<AppActions />
-		<RenderOptions />
+		<AppRenderOptions />
 	</div>
 
 	{#if images.hasOutputImages()}
