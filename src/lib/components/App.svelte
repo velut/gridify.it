a82af13c1fdddc263569e50652627f14f6724969
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-06-25 16:48:26 +0200

    refactor: rename app components

---
+++ src/lib/components/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { getAppStateContext } from '$lib/app-state.svelte';
+	import AppActions from '$lib/components/AppActions.svelte';
+	import AppPreview from '$lib/components/AppPreview.svelte';
+	import AppPreviewPlaceholder from '$lib/components/AppPreviewPlaceholder.svelte';
+	import AppRenderOptions from '$lib/components/AppRenderOptions.svelte';
+
+	let { images } = getAppStateContext();
+</script>
+
+<main class="grid gap-4 overflow-y-auto p-4 lg:grid-cols-[auto_minmax(0,1fr)]">
+	<div class="grid gap-4 overflow-y-auto sm:grid-cols-[auto_auto]">
+		<AppActions />
+		<AppRenderOptions />
+	</div>
+
+	{#if images.hasOutputImages()}
+		<AppPreview />
+	{:else}
+		<AppPreviewPlaceholder />
+	{/if}
+</main>
