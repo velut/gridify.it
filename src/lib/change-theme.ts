62030b982a24b0c069f8a18998d41438d42ef888
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-07-04 18:11:07 +0200

    feat: add theme changer button

--- src/lib/change-theme.ts
+++
@@ -1,9 +0,0 @@
-export function changeTheme() {
-	if (document.documentElement.classList.contains('dark')) {
-		localStorage.theme = 'light';
-		document.documentElement.classList.remove('dark');
-	} else {
-		localStorage.theme = 'dark';
-		document.documentElement.classList.add('dark');
-	}
-}
---
+++ src/lib/components/ThemeButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import MaterialSymbolsContrast from '~icons/material-symbols/contrast';
+
+	function changeTheme() {
+		if (document.documentElement.classList.contains('dark')) {
+			localStorage.theme = 'light';
+			document.documentElement.classList.remove('dark');
+		} else {
+			localStorage.theme = 'dark';
+			document.documentElement.classList.add('dark');
+		}
+	}
+</script>
+
+<button type="button" class="btn-sm-icon-outline" title="Change theme" onclick={changeTheme}>
+	<MaterialSymbolsContrast class="size-4" />
+	<span class="sr-only">Change theme</span>
+</button>
