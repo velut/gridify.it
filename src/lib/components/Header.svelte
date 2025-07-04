95dafce51e4db8619657844f878da5beacc4c20e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-07-04 18:11:12 +0200

    feat: update header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,15 +3,23 @@
 	import GridifyButton from '$lib/components/GridifyButton.svelte';
 	import HelpButton from '$lib/components/HelpButton.svelte';
 	import OpenAppButton from '$lib/components/OpenAppButton.svelte';
+	import ThemeButton from '$lib/components/ThemeButton.svelte';
 </script>
 
 <header class="border-b">
 	<div class="flex items-center justify-between gap-2 p-4">
 		<GridifyButton />
-		{#if page.route.id === '/'}
-			<OpenAppButton />
-		{:else if page.route.id === '/app'}
-			<HelpButton />
-		{/if}
+
+		<div class="flex items-center gap-4">
+			<ThemeButton />
+
+			<div class="bg-border h-8 w-[1px]"></div>
+
+			{#if page.route.id === '/app'}
+				<HelpButton />
+			{:else}
+				<OpenAppButton />
+			{/if}
+		</div>
 	</div>
 </header>
