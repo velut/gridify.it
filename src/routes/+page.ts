de656a2d6a5abaf962fa57059649dec5bb0fdb1f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-07-09 17:50:25 +0200

    chore: update homepage title

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = () => ({
-	title: 'Awesome grids for your images',
+	title: 'Free online grid maker from images',
 	description:
 		'Add a customizable grid to your images, photos, reference pictures, or pixel art designs. Use various grid types and line colors, resize images, and round the corners of grid cells.'
 });
