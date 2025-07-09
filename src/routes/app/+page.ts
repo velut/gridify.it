7c1c8a1598b6b125ec7df44ae6e8c599b0616434
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-07-09 17:48:33 +0200

    chore: update app page title

--- src/routes/app/+page.ts
+++ src/routes/app/+page.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = () => ({
-	title: 'Awesome grids for your images',
+	title: 'Grid editor',
 	description:
 		'Add a customizable grid to your images, photos, reference pictures, or pixel art designs. Use various grid types and line colors, resize images, and round the corners of grid cells.'
 });
