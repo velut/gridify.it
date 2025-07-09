0c0a05536dda2c23b499aaabb631cb720bfe2c0a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-07-09 17:45:50 +0200

    chore: update help page description

--- src/routes/help/+page.ts
+++ src/routes/help/+page.ts
@@ -2,5 +2,5 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = () => ({
 	title: 'Help',
-	description: 'Help section, instructions, and frequently asked questions about gridify.it'
+	description: 'Help section and instructions on how to use gridify.it'
 });
