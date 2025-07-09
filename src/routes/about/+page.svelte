8af52bdab6a3dacd63468c31dc41a2ebd19d0f4c
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-07-09 16:27:11 +0200

    feat: update help and about page layouts

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
 	import AboutArticle from '$lib/components/AboutArticle.svelte';
-	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<div class="grid min-h-screen grid-rows-[minmax(0,1fr)_auto]">
-	<main class="grid place-items-center p-4">
-		<AboutArticle />
-	</main>
-
-	<Footer />
+<div class="grid place-items-center">
+	<AboutArticle />
 </div>
--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-	import Footer from '$lib/components/Footer.svelte';
 	import HelpArticle from '$lib/components/HelpArticle.svelte';
 </script>
 
-<div class="grid min-h-screen grid-rows-[minmax(0,1fr)_auto]">
-	<main class="grid place-items-center p-4">
-		<HelpArticle />
-	</main>
-
-	<Footer />
+<div class="grid place-items-center">
+	<HelpArticle />
 </div>
