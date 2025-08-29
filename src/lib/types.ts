d9d8d6e345150ce756bc627a09e839d495c607f6
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-08-29 17:00:47 +0200

    feat: use zod to define schemas and infer types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,31 @@
-export type Image = {
-	file: File;
-	url: string;
-};
+import * as z from 'zod';
+
+export const Image = z.object({ file: z.file(), url: z.string() });
+export type Image = z.infer<typeof Image>;
+
+export const GridFilter = z.object({
+	kind: z.literal('grid'),
+	opts: z.object({
+		type: z.literal(['full', 'lines', 'border', 'none']).catch('full'),
+		color: z
+			.string()
+			.regex(/^#(?:[0-9a-f]{3}){1,2}$/i)
+			.catch('#000000'),
+		lines: z.object({ size: z.int().positive().catch(1) }),
+		cell: z.object({
+			shape: z.literal(['square', 'rectangle']).catch('square'),
+			width: z.int().positive().catch(1),
+			height: z.int().positive().catch(1),
+			scale: z.int().positive().catch(1),
+			cornerRadius: z.int().nonnegative().catch(0)
+		}),
+		opacity: z.literal(['preserve', 'opaque'])
+	})
+});
+export type GridFilter = z.infer<typeof GridFilter>;
+
+export const Filter = z.union([GridFilter]);
+export type Filter = z.infer<typeof Filter>;
 
 export type RenderOptions = {
 	grid: {
@@ -29,6 +53,7 @@ export type GridCellShape = 'square' | 'rectangle';
 
 export type PixelOpacity = 'preserve' | 'opaque';
 
-export type PreviewMode = 'pixel-art' | 'high-res';
+export const PreviewMode = z.literal(['pixel-art', 'high-res']);
+export type PreviewMode = z.infer<typeof PreviewMode>;
 
 export type ImagesRenderState = 'original' | 'rendered';
