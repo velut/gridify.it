52a2b66dfaf859c3e29b97fbc617c506ecd442a8
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-11 00:38:29 +0200

    feat: add AppColumn

---
+++ src/lib/components/AppColumn.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet;
+	};
+
+	let { children }: Props = $props();
+</script>
+
+<div
+	class="p-4 overflow-y-auto [scrollbar-width:thin] [scrollbar-color:_var(--scrollbar-thumb)_var(--scrollbar-track)]"
+>
+	{@render children()}
+</div>
