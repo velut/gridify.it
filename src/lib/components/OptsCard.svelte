80bad654964295644cb70ef8e5444ac5a523baec
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-11 00:16:40 +0200

    feat: add OptsCard

---
+++ src/lib/components/OptsCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		title: string;
+		children: Snippet;
+	};
+
+	let { title, children }: Props = $props();
+</script>
+
+<div class="rounded-xl border">
+	<div class="border-b flex items-center justify-center text-sm px-6 py-3 font-medium">
+		{title}
+	</div>
+	<div class="grid gap-4 p-6">
+		{@render children()}
+	</div>
+</div>
