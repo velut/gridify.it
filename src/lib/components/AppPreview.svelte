5febf3ae2318e6070b4650196c615c50ba565b01
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-11 01:21:21 +0200

    feat: update AppPreview

--- src/lib/components/AppPreview.svelte
+++ src/lib/components/AppPreview.svelte
@@ -3,12 +3,13 @@
 	import PreviewGallery from '$lib/components/PreviewGallery.svelte';
 	import PreviewMode from '$lib/components/PreviewMode.svelte';
 	import PreviewPlaceholder from '$lib/components/PreviewPlaceholder.svelte';
+	import AppColumn from './AppColumn.svelte';
 
 	const { images } = getAppStateContext();
 </script>
 
-<div class="card overflow-y-auto">
-	<section class="h-full">
+<AppColumn>
+	<div class="h-full">
 		{#if images.hasOutputImages()}
 			<div class="grid h-full grid-rows-[auto_minmax(0,1fr)] gap-6">
 				<PreviewMode />
@@ -17,5 +18,5 @@
 		{:else}
 			<PreviewPlaceholder />
 		{/if}
-	</section>
-</div>
+	</div>
+</AppColumn>
