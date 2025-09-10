8a36065dfb8c7142d4cf0d78668e342951f61e95
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-10 16:54:54 +0200

    feat: update RenderOptsState

---
+++ src/lib/state/render-opts-state.svelte.ts
@@ -0,0 +1,34 @@
+import { RenderOpts } from '$lib/types';
+import { deepEqual } from 'fast-equals';
+
+const defaultOpts = {
+	palette: {
+		type: 'original'
+	},
+	grid: {
+		type: 'full',
+		color: '#000000',
+		lines: { size: '1' },
+		cell: { shape: 'square', width: '1', height: '1', scale: '1', cornerRadius: '0' }
+	}
+};
+
+export class RenderOptsState {
+	opts = $state(structuredClone(defaultOpts));
+
+	toRenderOpts(): RenderOpts {
+		return RenderOpts.parse($state.snapshot(this.opts));
+	}
+
+	reset() {
+		this.opts = structuredClone(defaultOpts);
+	}
+
+	isDefault(): boolean {
+		return deepEqual($state.snapshot(this.opts), defaultOpts);
+	}
+
+	isGridTypeNone(): boolean {
+		return this.opts.grid.type === 'none';
+	}
+}
