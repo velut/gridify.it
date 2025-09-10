8be321837194e328520e6706a93b9d0712335023
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-11 00:44:35 +0200

    feat: add PaletteOpts

---
+++ src/lib/components/PaletteOpts.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { getAppStateContext } from '$lib/state/app-state.svelte';
+	import MaterialSymbolsPaletteOutline from '~icons/material-symbols/palette-outline';
+	import FormField from '$lib/components/FormField.svelte';
+	import OptsCard from '$lib/components/OptsCard.svelte';
+
+	const {
+		renderOpts: { opts }
+	} = getAppStateContext();
+</script>
+
+<OptsCard title="Palette options">
+	<FormField>
+		<label for="palette-type">
+			<MaterialSymbolsPaletteOutline class="size-4" />
+			Palette
+		</label>
+		<select id="palette-type" class="w-full" bind:value={opts.palette.type}>
+			<option value="original">Original colors</option>
+			<option value="opaque">Original colors with full opacity</option>
+			<option value="binary">Black and white</option>
+			<option value="grayscale">Grayscale</option>
+		</select>
+	</FormField>
+</OptsCard>
