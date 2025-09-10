f9918859e00a2a90612fa242e4bbc90e3f15a572
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-11 00:15:56 +0200

    feat: add horizontal option to Divider

--- src/lib/components/Divider.svelte
+++ src/lib/components/Divider.svelte
@@ -1,1 +1,13 @@
-<div class="bg-border h-[1px]"></div>
+<script lang="ts">
+	type Props = {
+		horizontal?: boolean;
+	};
+
+	let { horizontal }: Props = $props();
+</script>
+
+{#if horizontal}
+	<div class="bg-border w-[1px]"></div>
+{:else}
+	<div class="bg-border h-[1px]"></div>
+{/if}
