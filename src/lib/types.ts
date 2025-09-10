e5bb603d3358784953dfcd274542b1977188bfc1
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-10 16:53:58 +0200

    feat: add RenderOpts type and remove other types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,68 +3,33 @@ import * as z from 'zod';
 export const Image = z.object({ file: z.file(), url: z.string() });
 export type Image = z.infer<typeof Image>;
 
-export const OriginalFilter = z.object({
-	kind: z.literal('original')
+export const PaletteOpts = z.object({
+	type: z.literal(['original', 'opaque', 'binary', 'grayscale']).catch('original')
 });
+export type PaletteOpts = z.infer<typeof PaletteOpts>;
 
-export const GridFilter = z.object({
-	kind: z.literal('grid'),
-	opts: z.object({
-		type: z.literal(['full', 'lines', 'border', 'none']).catch('full'),
-		color: z
-			.string()
-			.regex(/^#(?:[0-9a-f]{3}){1,2}$/i)
-			.catch('#000000'),
-		lines: z.object({ size: z.coerce.number().int().positive().catch(1) }),
-		cell: z.object({
-			shape: z.literal(['square', 'rectangle']).catch('square'),
-			width: z.coerce.number().int().positive().catch(1),
-			height: z.coerce.number().int().positive().catch(1),
-			scale: z.coerce.number().int().positive().catch(1),
-			cornerRadius: z.coerce.number().int().nonnegative().catch(0)
-		}),
-		opacity: z.literal(['preserve', 'opaque'])
+export const GridOpts = z.object({
+	type: z.literal(['full', 'lines', 'border', 'none']).catch('full'),
+	color: z
+		.string()
+		.regex(/^#(?:[0-9a-f]{3}){1,2}$/i)
+		.catch('#000000'),
+	lines: z.object({ size: z.coerce.number().int().positive().catch(1) }),
+	cell: z.object({
+		shape: z.literal(['square', 'rectangle']).catch('square'),
+		width: z.coerce.number().int().positive().catch(1),
+		height: z.coerce.number().int().positive().catch(1),
+		scale: z.coerce.number().int().positive().catch(1),
+		cornerRadius: z.coerce.number().int().nonnegative().catch(0)
 	})
 });
-export type GridFilter = z.infer<typeof GridFilter>;
+export type GridOpts = z.infer<typeof GridOpts>;
 
-export const Filter = z.union([OriginalFilter, GridFilter]);
-export type Filter = z.infer<typeof Filter>;
-
-export const RenderItem = z.object({
-	active: z.boolean(),
-	filter: Filter,
-	images: z.array(Image)
+export const RenderOpts = z.object({
+	palette: PaletteOpts,
+	grid: GridOpts
 });
-export type RenderItem = z.infer<typeof RenderItem>;
-
-export type RenderOptions = {
-	grid: {
-		type: GridType;
-		color: string;
-		lines: {
-			size: number;
-		};
-		cell: {
-			shape: GridCellShape;
-			width: number;
-			height: number;
-			scale: number;
-			cornerRadius: number;
-		};
-	};
-	opacity: PixelOpacity;
-};
-
-export type RenderOptionsGrid = RenderOptions['grid'];
-
-export type GridType = 'full' | 'lines' | 'border' | 'none';
-
-export type GridCellShape = 'square' | 'rectangle';
-
-export type PixelOpacity = 'preserve' | 'opaque';
+export type RenderOpts = z.infer<typeof RenderOpts>;
 
 export const PreviewMode = z.literal(['pixel-art', 'high-res']);
 export type PreviewMode = z.infer<typeof PreviewMode>;
-
-export type ImagesRenderState = 'original' | 'rendered';
