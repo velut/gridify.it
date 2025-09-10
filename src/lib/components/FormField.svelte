7f4ca22dc40bb3c740866bb2ed940c398cc1d671
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-11 00:16:09 +0200

    feat: add FormField

---
+++ src/lib/components/FormField.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet;
+	};
+
+	let { children }: Props = $props();
+</script>
+
+<div class="grid gap-2">
+	{@render children()}
+</div>
