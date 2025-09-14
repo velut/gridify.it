2491cdba71d530fe78196c1850b962a8de3ecd1a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-15 01:15:21 +0200

    feat: update render button

--- src/lib/components/RenderImagesButton.svelte
+++ src/lib/components/RenderImagesButton.svelte
@@ -2,26 +2,26 @@
 	import { getAppState } from '$lib/state/app-state.svelte';
 	import MaterialSymbolsAnimatedImagesRounded from '~icons/material-symbols/animated-images-rounded';
 
-	const { images } = getAppState();
+	const { render } = getAppState();
 	let button: HTMLButtonElement;
-
-	function handleHotkey(e: KeyboardEvent) {
-		if (e.ctrlKey && e.key === 'Enter' && !button.disabled) {
-			e.preventDefault();
-			button.click();
-		}
-	}
 </script>
 
-<svelte:window onkeydown={handleHotkey} />
+<svelte:window
+	onkeydown={(event) => {
+		if (event.ctrlKey && event.key === 'Enter' && !button.disabled) {
+			event.preventDefault();
+			button.click();
+		}
+	}}
+/>
 
 <button
 	type="submit"
 	form="render-options-form"
 	class="btn"
-	disabled={!images.hasInputImages()}
-	bind:this={button}
 	title="Render images [Ctrl+Enter]"
+	bind:this={button}
+	disabled={!render.hasImages()}
 >
 	<MaterialSymbolsAnimatedImagesRounded class="size-4" />
 	Render images
