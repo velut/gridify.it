10536f6fdf695264d0c95b52f7c701c1e563a784
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-19 03:11:05 +0200

    feat: add rgbColorId

---
+++ src/lib/render/rgb-color-id.ts
@@ -0,0 +1,10 @@
+import type { RgbColor } from '$lib/types';
+
+export function rgbColorId({ r, g, b }: RgbColor): number {
+	// Unique ID for RGB colors.
+	// RRR 000 000 +
+	//     GGG 000 +
+	//         BBB =
+	// RRR GGG BBB
+	return r * 1_000_000 + g * 1_000 + b;
+}
