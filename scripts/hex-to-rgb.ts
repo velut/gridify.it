c00e01b86276f9894e1804229ea1f5fd6247a432
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-21 20:56:17 +0200

    refactor: update palette parsing

--- scripts/hex-to-rgb.ts
+++ scripts/hex-to-rgb.ts
@@ -40,11 +40,9 @@ function importPalette(palette: string): string {
 	return palette
 		.trim()
 		.split('\n')
-		.map((line) => line.trim())
-		.filter((line) => line.length === 6)
-		.map((hex) => {
-			const { red, green, blue } = hexRgb(hex);
-			return `[${red}, ${green}, ${blue}],`;
+		.map((line) => {
+			const [r, g, b] = hexRgb(line.trim(), { format: 'array' });
+			return `[${r}, ${g}, ${b}],`;
 		})
 		.join('\n');
 }
--- src/lib/render/import-palette.ts
+++ src/lib/render/import-palette.ts
@@ -5,10 +5,8 @@ export function importPalette(palette: string): RgbColor[] {
 	return palette
 		.trim()
 		.split('\n')
-		.map((line) => line.trim())
-		.filter((line) => line.length === 6)
-		.map((hex) => {
-			const { red, green, blue } = hexRgb(hex);
-			return [red, green, blue];
+		.map((line) => {
+			const [r, g, b] = hexRgb(line.trim(), { format: 'array' });
+			return [r, g, b];
 		});
 }
