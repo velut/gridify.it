4da66b5533fdd3e9b57b18013937a3b0c25807b6
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-22 02:10:17 +0200

    feat: update importPalette

--- src/lib/render/import-palette.ts
+++ src/lib/render/import-palette.ts
@@ -2,17 +2,23 @@ import { RgbColor } from '$lib/types';
 import hexRgb from 'hex-rgb';
 
 export function importPalette(palette: string): RgbColor[] {
-	return palette
+	const lines = palette
 		.trim()
 		.split('\n')
-		.map((line) => {
-			try {
-				const [r, g, b] = hexRgb(line.trim(), { format: 'array' });
-				return [r, g, b];
-			} catch {
-				// Return a magenta color, #f00baa, when the line is not a correct color
-				// so errors can be spotted easily.
-				return [240, 11, 170];
-			}
-		});
+		.map((line) => line.trim().toLowerCase());
+	const uniqueLines = [...new Set(lines)];
+
+	let colors: RgbColor[] = [];
+	for (const line of uniqueLines) {
+		try {
+			const [r, g, b] = hexRgb(line, { format: 'array' });
+			colors.push([r, g, b]);
+		} catch {
+			// `hexRgb` throws when it cannot parse a color.
+			// Return an "error" palette with a single magenta color (#f00baa)
+			// to signal that the imported palette contains incorrect colors.
+			return [[240, 11, 170]];
+		}
+	}
+	return colors;
 }
