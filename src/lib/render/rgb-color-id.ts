ade40dd4b31658cb18f50692a1592ec2f9b84ab6
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-23 01:55:41 +0200

    feat: use bitwise ops for rgb color id

--- src/lib/render/rgb-color-id.ts
+++ src/lib/render/rgb-color-id.ts
@@ -1,10 +1,6 @@
 import type { RgbColor } from '$lib/types';
 
 export function rgbColorId([r, g, b]: RgbColor): number {
-	// Unique ID for RGB colors.
-	// RRR 000 000 +
-	//     GGG 000 +
-	//         BBB =
-	// RRR GGG BBB
-	return r * 1_000_000 + g * 1_000 + b;
+	// Get unique ID for RGB colors by packing 8 bit components into a single integer.
+	return (r << 16) | (g << 8) | b;
 }
