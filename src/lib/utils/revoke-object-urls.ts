93001e1e5b2789886dff5d028157085946948456
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-25 02:51:37 +0200

    feat: update revokeObjectUrls

--- src/lib/utils/revoke-object-urls.ts
+++ src/lib/utils/revoke-object-urls.ts
@@ -1,11 +1,8 @@
-import type { AppImage } from '$lib/types';
-
-export function revokeObjectUrls(images: AppImage[]) {
-	if (images.length === 0) return;
-	const urls = images.map(({ url }) => url);
+export function revokeObjectUrls(urls: string[]) {
+	if (!urls.length) return;
 	setTimeout(() => {
 		for (const url of urls) {
 			URL.revokeObjectURL(url);
 		}
-	}, 10_000);
+	}, 30_000);
 }
