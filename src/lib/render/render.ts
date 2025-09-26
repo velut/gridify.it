a8f444a720c97bc83d28a214d4b2861672089a0f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-26 20:45:28 +0200

    feat: update render and render worker

--- src/lib/render/render.ts
+++ src/lib/render/render.ts
@@ -1,22 +1,17 @@
 import {
-	AppImageBuffer,
 	RenderWorkerInput,
 	RenderWorkerOutput,
 	RenderWorkerOutputData,
-	type AppBitmap,
-	type AppImage,
-	type RenderOpts
+	type AppImage
 } from '$lib/types';
-import pMap, { pMapSkip } from 'p-map';
 
 // Create worker later to prevent using DOM-only `Worker` on the server.
 let worker: Worker | undefined = undefined;
 
-export async function render(images: AppImage[], opts: RenderOpts): Promise<AppImage[]> {
+export async function render(input: RenderWorkerInput): Promise<AppImage[]> {
 	ensureWorker();
-	const bitmaps = await imagesToBitmaps(images);
-	const { buffers } = await renderOnWorker({ bitmaps, opts });
-	return await buffersToImages(buffers);
+	const { images } = await renderOnWorker(input);
+	return images;
 }
 
 function ensureWorker() {
@@ -24,36 +19,7 @@ function ensureWorker() {
 	worker = new Worker(new URL('./render.worker.ts', import.meta.url), { type: 'module' });
 }
 
-async function imagesToBitmaps(images: AppImage[]): Promise<AppBitmap[]> {
-	return await pMap(
-		images,
-		async ({ id, file, url }) => {
-			const filename = file.name;
-			try {
-				const bitmap = await window.createImageBitmap(file);
-				return { id, filename, bitmap };
-			} catch {
-				// Workaround as `createImageBitmap` doesn't support some image types like SVG.
-				try {
-					const img = new Image();
-					img.src = url;
-					await img.decode();
-					const canvas = new OffscreenCanvas(img.width, img.height);
-					const ctx = canvas.getContext('2d')!;
-					ctx.drawImage(img, 0, 0);
-					const bitmap = canvas.transferToImageBitmap();
-					return { id, filename, bitmap };
-				} catch (err) {
-					console.error(err);
-					return pMapSkip;
-				}
-			}
-		},
-		{ concurrency: 4 }
-	);
-}
-
-async function renderOnWorker(workerInput: RenderWorkerInput): Promise<RenderWorkerOutputData> {
+async function renderOnWorker(input: RenderWorkerInput): Promise<RenderWorkerOutputData> {
 	return await new Promise((resolve, reject) => {
 		// Use channels to enable async/await communication with worker.
 		// See https://advancedweb.hu/how-to-use-async-await-with-postmessage/.
@@ -71,25 +37,6 @@ async function renderOnWorker(workerInput: RenderWorkerInput): Promise<RenderWor
 		};
 
 		// Send message to worker.
-		const transferBitmaps = workerInput.bitmaps.map(({ bitmap }) => bitmap);
-		worker!.postMessage(workerInput, [port2, ...transferBitmaps]);
+		worker!.postMessage(input, [port2]);
 	});
-}
-
-async function buffersToImages(buffers: AppImageBuffer[]): Promise<AppImage[]> {
-	return await pMap(
-		buffers,
-		async ({ id, filename, buffer }) => {
-			try {
-				const name = `${filename}_${id}.png`;
-				const file = new File([buffer], name, { type: 'image/png' });
-				const url = URL.createObjectURL(file);
-				return { id, file, url };
-			} catch (err) {
-				console.error(err);
-				return pMapSkip;
-			}
-		},
-		{ concurrency: 4 }
-	);
 }
--- src/lib/render/render.worker.ts
+++ src/lib/render/render.worker.ts
@@ -1,19 +1,74 @@
-import { renderBitmaps } from '$lib/render/render-bitmaps';
-import { RenderWorkerInput, type RenderWorkerOutputData } from '$lib/types';
+import { applyGrid } from '$lib/render/apply-grid';
+import { applyPalette } from '$lib/render/apply-palette';
+import { AppImage, RenderOpts, RenderWorkerCache, RenderWorkerInput } from '$lib/types';
+import { nanoid } from '$lib/utils/nanoid';
+import pMap, { pMapSkip } from 'p-map';
+
+// Cache files with their decoded images already painted on a canvas.
+let cache: RenderWorkerCache = [];
 
 self.addEventListener('message', async (event) => {
 	const port = event.ports[0]!;
 	try {
-		const data = await work(RenderWorkerInput.parse(event.data));
-		const transferBuffers = data.buffers.map(({ buffer }) => buffer);
-		port.postMessage({ status: 'ok', data }, [...transferBuffers]);
+		const { opts, files } = RenderWorkerInput.parse(event.data);
+		await updateCache(files);
+		const images = await render(opts);
+		port.postMessage({ status: 'ok', data: { images } });
 	} catch (err) {
 		console.error(err);
 		port.postMessage({ status: 'err', error: err });
 	}
 });
 
-async function work(workerInput: RenderWorkerInput): Promise<RenderWorkerOutputData> {
-	const buffers = await renderBitmaps(workerInput);
-	return { buffers };
+async function updateCache(files?: File[]) {
+	if (!files) return;
+	cache = await pMap(
+		files,
+		async (file) => {
+			try {
+				const bitmap = await self.createImageBitmap(file);
+				const canvas = new OffscreenCanvas(bitmap.width, bitmap.height);
+				const ctx = canvas.getContext('2d')!;
+				ctx.imageSmoothingEnabled = false;
+				ctx.drawImage(bitmap, 0, 0);
+				return { file, canvas };
+			} catch (err) {
+				console.error(err);
+				return pMapSkip;
+			}
+		},
+		{ concurrency: 4 }
+	);
+}
+
+async function render(opts: RenderOpts): Promise<AppImage[]> {
+	return await pMap(
+		cache,
+		async ({ file: originalFile, canvas: originalCanvas }) => {
+			try {
+				// Copy original canvas.
+				let canvas = new OffscreenCanvas(originalCanvas.width, originalCanvas.height);
+				const ctx = canvas.getContext('2d')!;
+				ctx.imageSmoothingEnabled = false;
+				ctx.drawImage(originalCanvas, 0, 0);
+
+				// Apply opts.
+				canvas = applyPalette(canvas, opts.palette);
+				canvas = applyGrid(canvas, opts.grid);
+
+				// Return image.
+				const blob = await canvas.convertToBlob({ type: 'image/png', quality: 1 });
+				const id = nanoid();
+				const name = `${originalFile.name}_${id}.png`;
+				const file = new File([blob], name, { type: 'image/png' });
+				const url = URL.createObjectURL(file);
+				const { width, height } = canvas;
+				return { id, file, url, width, height };
+			} catch (err) {
+				console.error(err);
+				return pMapSkip;
+			}
+		},
+		{ concurrency: 1 }
+	);
 }
