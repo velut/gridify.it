a28a4d2f8c9590f62f6240ec6ae0185447511019
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-09-26 20:53:26 +0200

    feat: remove support for uploading svg images

--- src/lib/utils/get-image-files.ts
+++ src/lib/utils/get-image-files.ts
@@ -3,5 +3,10 @@ import { getFiles } from 'event-to-files';
 export async function getImageFiles(event: Event): Promise<File[]> {
 	return (await getFiles(event))
 		.map(({ file }) => file)
-		.filter(({ type }) => type.trim().toLowerCase().startsWith('image/'));
+		.filter((file) => {
+			const type = file.type.trim().toLowerCase();
+			const isImage = type.startsWith('image/');
+			const isSvg = type === 'image/svg+xml';
+			return isImage && !isSvg;
+		});
 }
