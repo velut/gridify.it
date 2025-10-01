4147cfbaa9b7c0d619bdeb52053f704dcdaa2e92
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-10-01 02:41:46 +0200

    feat: add PreviewTip

---
+++ src/lib/components/PreviewTip.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import MaterialSymbolsImageSearchOutlineRounded from '~icons/material-symbols/image-search-outline-rounded';
+</script>
+
+<div class="text-muted-foreground text-sm flex items-center gap-2 text-balance">
+	<MaterialSymbolsImageSearchOutlineRounded class="size-4 flex-none" />
+	The preview may contain visual artifacts, click on the image to display it at full size
+</div>
