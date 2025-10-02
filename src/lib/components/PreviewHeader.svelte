b493f551f68ef3626b506bba43bd2486e354fa06
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-10-02 17:19:31 +0200

    chore: formatting

--- src/lib/components/PreviewHeader.svelte
+++ src/lib/components/PreviewHeader.svelte
@@ -10,27 +10,27 @@
 	let id = $derived(images[index].id);
 </script>
 
-<div class="flex flex-col xl:flex-row gap-4">
+<div class="flex flex-col gap-4 xl:flex-row">
 	<div>
 		<label for="preview-image-id" class="sr-only">Select an image to display</label>
-		<select id="preview-image-id" class="w-full select" bind:value={id}>
+		<select id="preview-image-id" class="select w-full" bind:value={id}>
 			{#each images as image (image.id)}
 				<option value={image.id}>{image.originalFile.name}</option>
 			{/each}
 		</select>
 	</div>
 
-	<div class="hidden xl:block bg-border h-9 w-[1px]"></div>
-	<div class="flex flex-col sm:flex-row gap-4">
+	<div class="bg-border hidden h-9 w-[1px] xl:block"></div>
+	<div class="flex flex-col gap-4 sm:flex-row">
 		<div>
 			<label for="preview-mode" class="sr-only"> Select preview mode </label>
-			<select id="preview-mode" class="w-full select" bind:value={preview.mode}>
+			<select id="preview-mode" class="select w-full" bind:value={preview.mode}>
 				<option value="pixel-art">Show as pixel art</option>
 				<option value="hi-res">Show as hi-res image</option>
 			</select>
 		</div>
 
-		<div class="hidden sm:block bg-border h-9 w-[1px]"></div>
+		<div class="bg-border hidden h-9 w-[1px] sm:block"></div>
 		<div class="flex items-center gap-4">
 			<span class="btn-outline pointer-events-none">{image.width}x{image.height}</span>
 
--- src/lib/components/PreviewTip.svelte
+++ src/lib/components/PreviewTip.svelte
@@ -2,7 +2,7 @@
 	import MaterialSymbolsImageSearchOutlineRounded from '~icons/material-symbols/image-search-outline-rounded';
 </script>
 
-<div class="text-muted-foreground text-sm flex items-center gap-2 text-balance">
+<div class="text-muted-foreground flex items-center gap-2 text-sm text-balance">
 	<MaterialSymbolsImageSearchOutlineRounded class="size-4 flex-none" />
 	The preview may contain visual artifacts, click on the image to display it at full size
 </div>
