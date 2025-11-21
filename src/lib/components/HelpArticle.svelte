d250039f45c67bb579ce60fd0a9e364e1c71094a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-11-21 20:23:50 +0100

    docs: update help page article

--- src/lib/components/HelpArticle.svelte
+++ src/lib/components/HelpArticle.svelte
@@ -16,7 +16,6 @@
 			<li><a href="#rendering-options">Rendering options</a></li>
 			<li><a href="#rendering-options-grid">Rendering options: Grid</a></li>
 			<li><a href="#rendering-options-grid-cells">Rendering options: Grid cells</a></li>
-			<li><a href="#rendering-options-opacity">Rendering options: Opacity</a></li>
 			<li><a href="#tips-for-pixel-art">Tips for pixel art</a></li>
 			<li><a href="#tips-for-high-resolution-images">Tips for high resolution images</a></li>
 			<li><a href="#other-questions">Other questions</a></li>
@@ -28,26 +27,24 @@
 
 		<h3>How do I upload images?</h3>
 		<p>
-			Click on the <strong>Upload images</strong> button to open a file picker dialog. You can also
-			directly <strong>drag and drop</strong> the images on the app page.
+			You can click on the <strong>Upload images</strong> button to open a file picker dialog. You
+			can also directly <strong>drag and drop</strong> the images on the app page.
 		</p>
 
 		<h3>How do I remove uploaded images?</h3>
 		<p>
-			Click on the <strong>Remove images</strong> button to remove all uploaded images.
+			You can click on the <strong>Remove images</strong> button to remove all uploaded images. You can
+			also reload the app page to start from scratch.
 		</p>
 
 		<h3>Which image formats can I use?</h3>
 		<p>
-			The app supports most common image formats. For best results, use <strong>JPEG</strong>
-			and
-			<strong>PNG</strong> images.
+			The app supports most common raster image formats. For best results, you should use
+			<strong>JPEG</strong> and <strong>PNG</strong> images.
 		</p>
 
 		<h3>How many images can I upload?</h3>
-		<p>
-			<strong>As many as you want.</strong> You can upload a single image or many images together.
-		</p>
+		<p>You can upload a single image or multiple images together.</p>
 
 		<h3>Are my uploaded images kept private?</h3>
 		<p>
@@ -60,7 +57,8 @@
 
 		<h3>How do I download the rendered images?</h3>
 		<p>
-			Click on the <strong>Download images</strong> button to save the rendered images on your device.
+			You can click on the <strong>Download images</strong> button to save the rendered images on your
+			device.
 		</p>
 	</ArticleSection>
 
@@ -68,22 +66,34 @@
 		<h2 id="previewing-images">Previewing images</h2>
 
 		<h3>How do I preview images?</h3>
-		<p>After uploading your images, you will immediately see them in the preview area.</p>
+		<p>
+			After uploading your images, you will see them in the preview area. If you upload multiple
+			images, you can choose which one to display by selecting its filename at the top of the
+			preview area.
+		</p>
 
 		<h3>How do I preview an image at full size?</h3>
-		<p>Click on an image in the preview area to see it at full size in a new tab.</p>
+		<p>
+			You can click on an image in the preview area to see it at full size in a new browser tab.
+		</p>
 
 		<h3>How do I change the preview mode?</h3>
 		<p>
-			Click on the <strong>Preview mode</strong> setting in the preview area and select between
-			<strong>Pixel art</strong> (for pixel art, game sprites, and smaller images) and
-			<strong>High resolution images</strong> (for photos, drawings, and larger images).
+			You can choose between <strong>Show as pixel art</strong> (for pixel art, game sprites, and
+			smaller images) and <strong>Show as hi-res image</strong> (for photos, drawings, and larger images)
+			at the top of the preview area.
 		</p>
 
 		<h3>Why do image previews look weird?</h3>
 		<p>
-			Scaling in the preview area may cause image previews to contain visual artifacts; click on an
-			image to see it at full size in a new tab.
+			Scaling in the preview area may cause image previews to contain visual artifacts. You can
+			click on an image to see it at full size in a new browser tab.
+		</p>
+
+		<h3>What are the numbers at the top of the preview area?</h3>
+		<p>
+			At the top of the preview area, you can see the image dimensions in pixels (width x height)
+			and the image file size in bytes.
 		</p>
 	</ArticleSection>
 
@@ -92,19 +102,25 @@
 
 		<h3>How do I render images?</h3>
 		<p>
-			Click on the <strong>Render images</strong> button. You can also use the
-			<strong>Ctrl+Enter</strong> keyboard shortcut.
+			You can click on the <strong>Render images</strong> button. You can also use the
+			<kbd class="kbd">Ctrl + Enter</kbd> keyboard shortcut.
 		</p>
 
 		<h3>How do I undo the images render?</h3>
 		<p>
-			Click on the <strong>Undo render images</strong> button to restore images to their original
-			appearance. You can also use the <strong>Ctrl+Z</strong> keyboard shortcut.
+			You can click on the <strong>Undo</strong> button to restore images to their previous
+			appearance. You can also use the <kbd class="kbd">Ctrl + Z</kbd> keyboard shortcut.
+		</p>
+
+		<h3>How do I redo the images render?</h3>
+		<p>
+			You can click on the <strong>Redo</strong> button to repeat a previously undone render. You
+			can also use the <kbd class="kbd">Ctrl + Y</kbd> keyboard shortcut.
 		</p>
 
 		<h3>Why have the images been rendered incorrectly?</h3>
 		<p>
-			Bugs in browsers or graphical drivers may cause images to be rendered incorrectly. Try
+			Bugs in browsers or graphical drivers may cause images to be rendered incorrectly. You can try
 			changing the rendering options or using different browsers or devices to render the images.
 		</p>
 	</ArticleSection>
@@ -114,14 +130,15 @@
 
 		<h3>What are the rendering options?</h3>
 		<p>
-			The <strong>rendering options</strong> are the settings that determine how the grid will look like
-			when added to your images.
+			The <strong>rendering options</strong> are the settings that determine how the images will look
+			like after being rendered. You can use the rendering options to change the size and colors of images
+			and also add a custom grid.
 		</p>
 
 		<h3>How do I reset the rendering options?</h3>
 		<p>
-			Click on the <strong>Reset render options</strong> button to restore the rendering options to their
-			default values.
+			You can click on the <strong>Reset render options</strong> button to restore the rendering options
+			to their default values.
 		</p>
 	</ArticleSection>
 
@@ -129,13 +146,17 @@
 		<h2 id="rendering-options-grid">Rendering options: Grid</h2>
 
 		<h3>Which grid type should I choose?</h3>
-		<p>The app provides different grid types for you to use:</p>
+		<p>You can choose between five different grid types:</p>
 		<ul>
+			<li>
+				<p><strong>No grid</strong></p>
+				<p>Use this if you want to completely disable grid rendering.</p>
+			</li>
 			<li>
 				<p><strong>Full grid</strong></p>
 				<p>
 					Use this if you want the grid to have both the outer border and the inner grid lines
-					between the cells as shown below.
+					between grid cells as shown below.
 				</p>
 				<div class="not-prose grid place-items-start">
 					<div class="bg-card-foreground grid grid-cols-3 grid-rows-3 gap-px p-px">
@@ -146,10 +167,10 @@
 				</div>
 			</li>
 			<li>
-				<p><strong>Grid lines only</strong></p>
+				<p><strong>Only grid lines</strong></p>
 				<p>
-					Use this if you want only the inner grid lines between the cells without the outer grid
-					border as shown below.
+					Use this if you want only the inner grid lines between grid cells without the outer border
+					as shown below.
 				</p>
 				<div class="not-prose grid place-items-start">
 					<div class="bg-card-foreground grid grid-cols-3 grid-rows-3 gap-px">
@@ -160,10 +181,9 @@
 				</div>
 			</li>
 			<li>
-				<p><strong>Outer border only</strong></p>
+				<p><strong>Only outer border</strong></p>
 				<p>
-					Use this if you want only the outer grid border without the inner grid lines as shown
-					below.
+					Use this if you want only the outer border without the inner grid lines as shown below.
 				</p>
 				<div class="not-prose grid place-items-start">
 					<div class="bg-card-foreground grid grid-cols-3 grid-rows-3 p-px">
@@ -174,10 +194,10 @@
 				</div>
 			</li>
 			<li>
-				<p><strong>None</strong></p>
+				<p><strong>Invisible grid</strong></p>
 				<p>
-					Use this if you do not want any type of grid lines. This can be useful when you only want
-					to round the corners of cells as shown below.
+					Use this if you do not want any type of grid lines but still want to have grid cells. This
+					can be useful when you only want to round the corners of grid cells as shown below.
 				</p>
 				<div class="not-prose grid place-items-start">
 					<div class="grid grid-cols-3 grid-rows-3 p-px">
@@ -191,14 +211,16 @@
 
 		<h3>How do I change the color of the grid?</h3>
 		<p>
-			Click on the <strong>Grid color</strong> setting to choose a different color for the grid. The
-			selected color is also used to round cell corners even if the grid type is None.
+			You can click on the <strong>Grid color</strong> setting to choose a different color for the
+			grid. The selected color applies to the outer border and grid lines and is also used to round
+			the corners of grid cells when the grid type is
+			<strong>Invisible grid</strong>.
 		</p>
 
 		<h3>How do I change the grid lines size?</h3>
 		<p>
-			Click on the <strong>Grid lines size</strong> setting and type a value in pixels to choose how
-			thin or thick you want the outer border and the inner grid lines to be.
+			You can click on the <strong>Grid border and lines size</strong> setting and type a value in pixels
+			to choose how thin or thick you want the outer border and the inner grid lines to be.
 		</p>
 	</ArticleSection>
 
@@ -221,7 +243,7 @@
 
 		<h3>How do I change the type of grid cells?</h3>
 		<p>
-			Click on the <strong>Grid cell type</strong> setting and select between
+			You can click on the <strong>Grid cell type</strong> setting and choose between
 			<strong>Square</strong> to have grid cells with the same width and height (shown below on the
 			left) and
 			<strong>Rectangle</strong> to freely choose the grid cell sizes (shown below on the right).
@@ -247,14 +269,14 @@
 
 		<h3>How do I change the width of grid cells?</h3>
 		<p>
-			Click on the <strong>Grid cell width</strong> setting and type a value in pixels to choose how
-			wide you want the grid cells to be. This value will also be used for the height of grid cells
-			if the <strong>Grid cell type</strong> is set to <strong>Square</strong>.
+			You can click on the <strong>Grid cell width</strong> setting and type a value in pixels to
+			choose how wide you want the grid cells to be. This value is also be used for the height of
+			grid cells if the <strong>Grid cell type</strong> is set to <strong>Square</strong>.
 		</p>
 
 		<h3>How do I change the height of grid cells?</h3>
 		<p>
-			Click on the <strong>Grid cell type</strong> setting and select the
+			You can click on the <strong>Grid cell type</strong> setting and select the
 			<strong>Rectangle</strong> option, then click on the <strong>Grid cell height</strong> setting
 			and type a value in pixels to choose how tall you want the grid cells to be.
 		</p>
@@ -263,7 +285,7 @@
 		<p>
 			All grid cells have the exact same size except for the ones that do not fit in the image at
 			the right and bottom edges. Fox example, in the grid below the cells 3, 6, and 9 are smaller
-			and only fill the remaining space on the right.
+			and only fill the remaining space on the right border of the image.
 		</p>
 
 		<div class="not-prose grid place-items-start">
@@ -278,35 +300,17 @@
 
 		<h3>How do I scale up grid cells?</h3>
 		<p>
-			Click on the <strong>Grid cell scale</strong> setting and type a multiplier value to apply to the
-			grid cell sizes. For example, if you set the scale to 5, a grid cell that is 20x20 pixels in the
-			original image will be scaled up to 100x100 pixels in the rendered image.
+			You can click on the <strong>Grid cell scale</strong> setting and type a multiplier value to apply
+			to the grid cell sizes. For example, if you set the scale to 5, a grid cell that is 20x20 pixels
+			in the original image will be scaled up to 100x100 pixels in the rendered image.
 		</p>
 
 		<h3>How do I round the corners of grid cells?</h3>
 		<p>
-			Click on the <strong>Grid cell corner radius</strong> setting and type a value in pixels for
-			the radius to apply to the corners of grid cells. You may need to also scale up cells using
-			the <strong>Grid cell scale</strong> setting if they are too small to be rounded correctly.
-		</p>
-	</ArticleSection>
-
-	<ArticleSection>
-		<h2 id="rendering-options-opacity">Rendering options: Opacity</h2>
-
-		<h3>How do I change the transparency of pixels?</h3>
-		<p>
-			Click on the <strong>Transparent pixels</strong> setting and select between the
-			<strong>Preserve transparency</strong> option to keep the original pixel transparency in the
-			rendered image and the <strong>Fully opaque pixels</strong> option to make all pixels fully opaque
-			in the rendered image.
-		</p>
-
-		<h3>Why should I make the pixels fully opaque?</h3>
-		<p>
-			Preserving the original pixel transparency causes the image colors to be mixed with the grid
-			color. Changing the pixels to fully opaque prevents the mixing between the image colors and
-			the grid color.
+			You can click on the <strong>Grid cell corner radius</strong> setting and type a value in
+			pixels for the radius to apply to the corners of grid cells. You may need to also scale up
+			grid cells using the <strong>Grid cell scale</strong> setting if they are too small to be rounded
+			correctly.
 		</p>
 	</ArticleSection>
 
@@ -315,9 +319,9 @@
 
 		<h3>Use square, 1x1 pixels grid cells</h3>
 		<p>
-			If you want every pixel in the original pixel art image to be its own grid cell, use the
-			<strong>Square</strong> cell type and set the <strong>Grid cell width</strong> to be 1 pixel. These
-			are the default rendering options.
+			If you want every pixel in the original pixel art image to be its own grid cell, you should
+			use the <strong>Square</strong> grid cell type and set the <strong>Grid cell width</strong> to
+			be 1 pixel. These are the default values when rendering an image.
 		</p>
 
 		<h3>Scale up grid cells to distinguish them from grid lines</h3>
@@ -342,7 +346,7 @@
 
 		<h3>Use larger grid cells</h3>
 		<p>
-			If you are adding a grid to a high resolution image, try setting the
+			If you are adding a grid to a high resolution image, you can try setting the
 			<strong>Grid cell width</strong> to a higher value like 100 pixels. Otherwise, the grid will be
 			very fine and the rendered image will look like a darker version of the original image.
 		</p>
@@ -356,8 +360,8 @@
 		<h3>Render fewer images at a time</h3>
 		<p>
 			Adding grids to high resolution images, especially with smaller cell sizes, is a resource
-			intensive operation which may cause the app to become slow and unresponsive. If you are
-			rendering many high resolution images, try rendering them in smaller batches or one by one.
+			intensive operation which may cause the app to become slow and unresponsive. When processing
+			many high resolution images, you should try rendering them in smaller batches or one by one.
 		</p>
 	</ArticleSection>
 
