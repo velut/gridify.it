6cfb2e641596128038784857243934778894ee44
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2026-01-13 19:40:52 +0100

    chore: formatting

--- src/lib/components/HomepageHero.svelte
+++ src/lib/components/HomepageHero.svelte
@@ -4,7 +4,7 @@
 
 <div class="grid place-items-center gap-6 py-16">
 	<div class="flex flex-col items-center gap-4 text-center text-balance">
-		<h1 class="text-4xl md:text-5xl font-bold">Free online grid maker</h1>
+		<h1 class="text-4xl font-bold md:text-5xl">Free online grid maker</h1>
 		<p class="text-muted-foreground max-w-[65ch]">
 			Add a custom grid to your images, pixel art, and photos in seconds.
 		</p>
