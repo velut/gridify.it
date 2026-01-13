c3b8693c1e5f6b28f43f79b50bcea6902d85416a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2026-01-13 19:16:16 +0100

    feat: increase vertical padding in HomepageHero and add description

--- src/lib/components/HomepageHero.svelte
+++ src/lib/components/HomepageHero.svelte
@@ -2,12 +2,11 @@
 	import OpenAppButton from '$lib/components/OpenAppButton.svelte';
 </script>
 
-<div class="grid place-items-center gap-6 py-12">
+<div class="grid place-items-center gap-6 py-16">
 	<div class="flex flex-col items-center gap-4 text-center text-balance">
-		<h1 class="text-5xl font-bold">Free online grid maker</h1>
+		<h1 class="text-4xl md:text-5xl font-bold">Free online grid maker</h1>
 		<p class="text-muted-foreground max-w-[65ch]">
-			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, earum accusamus dolorem,
-			cum nam laudantium aut officiis.
+			Add a custom grid to your images, pixel art, and photos in seconds.
 		</p>
 	</div>
 
