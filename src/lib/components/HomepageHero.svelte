4098c8e54be827c7e363b89d3663e5bb06047e84
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2026-01-27 19:36:59 +0100

    feat: update HomepageHero

--- src/lib/components/HomepageHero.svelte
+++ src/lib/components/HomepageHero.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
-	import OpenAppButton from '$lib/components/OpenAppButton.svelte';
+	import MaterialSymbolsEditSquareOutlineRounded from '~icons/material-symbols/edit-square-outline-rounded';
 </script>
 
-<div class="grid place-items-center gap-6 py-16">
-	<div class="flex flex-col items-center gap-4 text-center text-balance">
-		<h1 class="text-4xl font-bold md:text-5xl">Free online grid maker</h1>
-		<p class="text-muted-foreground max-w-[65ch]">
-			Add a custom grid to your images, pixel art, and photos in seconds.
-		</p>
-	</div>
+<div class="grid place-items-center border-b px-4 py-6 md:py-12">
+	<div class="grid gap-6 place-items-center max-w-7xl">
+		<div class="grid grid-cols-1 gap-3 place-items-center">
+			<h1
+				class="text-4xl text-center text-balance font-bold md:text-5xl max-w-[10ch] md:max-w-none"
+			>
+				Free online grid maker
+			</h1>
+			<p class="text-muted-foreground max-w-[65ch] text-center text-balance">
+				Add a custom grid to your images, pixel art, and photos in seconds.
+			</p>
+		</div>
 
-	<OpenAppButton />
+		<a href="/app" class="btn-lg">
+			<MaterialSymbolsEditSquareOutlineRounded class="size-4" />
+			Open app
+		</a>
+	</div>
 </div>
